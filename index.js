7678f12bd7e6e3b3271aa303c96aef0a4d155b2a
khurshid-ali <md_khurshid@hotmail.com>
2018-09-05 16:27:52 -0300

    added mongoose db connection

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const config = require("config");
+const mongoose = require("mongoose");
 const homeRouter = require("./routers/homeRouter");
 const express = require("express");
 const app = express();
@@ -18,12 +19,24 @@ if (!config.get("jwtPrivateKey")) {
   process.exit(1);
 }
 
+//initialize mongoose.
+const mongooseUri = `mongodb+srv://${config.get("mongoDb.user")}:${config.get(
+  "mongoDb.password"
+)}@${config.get("mongoDb.server")}/${config.get("mongoDb.database")}`;
+console.log("connecting to:" + mongooseUri);
+mongoose.connect(mongooseUri);
+mongoose.connection
+  .on("error", console.error.bind(console, "connection error:"))
+  .once("open", function() {
+    console.log("connection to monbodb is open.");
+  });
+
 //middleware configurations
 
 //router configurations
+app.use("/", homeRouter);
 
 //Error Handler and loggers configurations
-app.use("/", homeRouter);
 
 //start the server.
 
